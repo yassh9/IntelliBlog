7e64f86c00af4ac29e367d86b7f3754b3847e7f0
Yash <ojhayash25@gmail.com>
2025-10-01 19:31:16 +0530

    Refactor Navbar component layout for improved structure and styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,10 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 '>
-        <img src={assets.logo} onClick={() => navigate('/')} alt="logo" className=" cursor-pointer w-32 sm:w-44" />
+        <div className="flex items-center gap-3" >
+          <img src="/favicon.svg" onClick={() => navigate('/')} alt="logo" className=" cursor-pointer " />
+        <h1 className="text-3xl font-bold">IntelliBlog</h1>
+        </div>
         <button onClick={() => navigate('/admin')} className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5">Login
             <img src={assets.arrow} alt="arrow" className="w-3" />
         </button>
