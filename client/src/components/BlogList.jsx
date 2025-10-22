5ca67f632363bebd8258aa8537013e47d82a0314
Yash <ojhayash25@gmail.com>
2025-10-22 18:55:59 +0530

    Add BlogCard component and integrate with BlogList; include motion for animations

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { blogCategories } from '../assets/assets'
-
+import { blog_data, blogCategories } from '../assets/assets'
+import BlogCard from './BlogCard.jsx'
+import {motion} from 'motion/react'
 const BlogList = () => {
   const [menu , setMenu] = React.useState('All')
   return (
@@ -12,16 +13,16 @@ const BlogList = () => {
             onClick={() => setMenu(item)}
              className={`cursor-pointer text-gray-500 ${menu === item && 'text-white px-4 pt-0.5'}`}>
                 {item}
-                {menu===item && <div className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'> </div>}
+                {menu===item && <motion.div layoutId="underline " transition={{type:'spring', stiffness:500 , damping:30}} className='absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full'> </motion.div>}
                 
             </button>
             </div>
         ))}
 
       </div>
-      <div>
-        {/* --- blog cards ---  */}
-      </div>
+      <div className='grid grid-cols-1 sm:grid-cpls-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40'>
+       {blog_data.filter((blog) => menu === 'All' ? true: blog.category === menu).map((blog) => (<BlogCard key={blog._id} blog={blog} />))}
+      </div>  
     </div>
   )
 }
